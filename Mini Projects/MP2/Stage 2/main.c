d9696d1d2a18f27082769828abdad3dd83f2424f
Sam Hart <wsdh500@york.ac.uk>
2019-10-25 12:36:57 +0100

    Progress towards clearing display properly.
    See commented-out code.

--- Mini Projects/MP2/Stage 2/main.c
+++ Mini Projects/MP2/Stage 2/main.c
@@ -21,6 +21,12 @@ I2C_M_SETUP_Type setup =
   .status = 0
 };
 
+void pins_i2c(void);
+void init_i2c(void);
+void clear_lcd(void);
+void write_lcd_pos(uint8_t character , uint8_t position);
+void write_i2c(void);
+
 void pins_i2c(void)
 {
   PINSEL_CFG_Type PinCfg;
@@ -43,7 +49,6 @@ void init_i2c(void)
 
 void init_lcd(void)
 {
-  Status result;
   uint32_t index;
   const uint32_t init_bytes[10] = {0x34 , 0x0c , 0x06 , 0x35 , 0x04 , 0x10 , 0x42 , 0x9f , 0x34 , 0x02};
   setup.sl_addr7bit = 0x3b;
@@ -51,17 +56,44 @@ void init_lcd(void)
   for(index = 0 ; index < 10 ; index++)
   {
     send[1] = init_bytes[index];
-    result = I2C_MasterTransferData(LPC_I2C1, &setup, I2C_TRANSFER_POLLING);
+    write_i2c();
   }
 }
 
 void clear_lcd(void)
 {
-  Status result;
+  /*
+   * 10.2.2.1 Clear_display - pg. 32/86 
+   * When using character set ‘R’, the following alternative instruction set has to be used:
+   * 1.  Switch display off (Display_ctl, bit D = 0).
+   * 2.  Write a blank pattern into all DDRAM addresses (Write_data).
+   * 3.  Switch display on (Display_ctl, bit D = 1).
+   *
+   * 10.2.2.4 Display_ctl - pg. 33/86
+   * control byte = 0
+   * command byte = 0x08 (off) 0x0C (on)
+   *
+   * blank patterns = 0xA0, 0x91, 0x9B
+   */
   uint32_t tick;
+  //uint32_t position;
   send[0] = 0x00;
+  /*
+  // Switch off display
+  send[1] = 0x08;
+  write_i2c();
+
+  // Write blank pattern into all DDRAM addresses
+  for(position = 0 ; position < 40 ; position++)
+    write_lcd_pos(0xA0 , position);
+
+  // Switch on display
+  send[1] = 0x0C;
+  write_i2c();
+  */
+  // Clear the display
   send[1] = 0x01;
-  result = I2C_MasterTransferData(LPC_I2C1, &setup, I2C_TRANSFER_POLLING);
+  write_i2c();
   tick = 0;
   while(tick++ < 1024)
     ;
@@ -69,14 +101,21 @@ void clear_lcd(void)
 
 void write_lcd_pos(uint8_t character , uint8_t position)
 {
-  Status result;
   send[0] = 0x00;
   send[1] = 0x80 + position;
-  result = I2C_MasterTransferData(LPC_I2C1, &setup, I2C_TRANSFER_POLLING);
+  write_i2c();
 
   send[0] = 0x40;
   send[1] = character;
+  write_i2c();
+}
+
+void write_i2c(void)
+{
+  Status result;
   result = I2C_MasterTransferData(LPC_I2C1, &setup, I2C_TRANSFER_POLLING);
+  if(!result)
+    write_usb_serial_blocking("ERROR\n\r" , 7);
 }
 
 // Entry point for the program
@@ -86,21 +125,23 @@ void main(void)
   int returncode;
   char result_string[11];
 
+  // initialise serial
   serial_init();
   write_usb_serial_blocking("LCD\n\r" , 5);
+
+  // initialise I2C and LCD
   init_i2c();
-
-  returncode = 0;
-
   init_lcd();
   clear_lcd();
 
-  sprintf(result_string, "%02X %02X %02X\n\r", setup.sl_addr7bit , *setup.tx_data , *(setup.tx_data+1));
+  returncode = 0;
+  returncode = sprintf(result_string, "%02X %02X %02X\n\r", setup.sl_addr7bit , *setup.tx_data , *(setup.tx_data+1));
   write_usb_serial_blocking(result_string , 10);
 
-  for(position = 0 ; position < 32 ; position++)
+  // write characters to all positions
+  for(position = 0 ; position < 16 ; position++)
   {
-    write_lcd_pos(0x14 , position);
+    write_lcd_pos(0x7F , position);
     sprintf(result_string, "%02X %02X %02X\n\r", setup.sl_addr7bit , *setup.tx_data , *(setup.tx_data+1));
     write_usb_serial_blocking(result_string , 10);
   }
