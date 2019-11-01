20597d49281772306fbb2c6b324d648a3d8197f7
Sam Hart <wsdh500@york.ac.uk>
2019-11-01 10:20:13 +0000

    Move lookup table, LUT, into the header file.

--- Mini Projects/MP2/Stage 3/keypad.c
+++ Mini Projects/MP2/Stage 3/keypad.c
@@ -9,33 +9,6 @@
 #include "i2c.h"
 #include "keypad.h"
 
-const uint8_t LUT[4][4] =
-{
-	{
-		0xAA , // *
-		0xB0 , // 0
-		0xA3 , // #
-		0xC4 , // D
-	} ,
-	{
-		0xB7 , // 7
-		0xB8 , // 8
-		0xB9 , // 9
-		0xC3 , // C
-	} ,
-	{
-		0xB4 , // 4
-		0xB5 , // 5
-		0xB6 , // 6
-		0xC2 , // B
-	} ,
-	{
-		0xB1 , // 1
-		0xB2 , // 2
-		0xB3 , // 3
-		0xC1 , // A
-	}
-};
 uint8_t k_send;
 uint8_t k_receive;
 I2C_M_SETUP_Type k_setup =
