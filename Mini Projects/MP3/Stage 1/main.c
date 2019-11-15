7b888554f5701b3d76a5d377c1eb2cb6817df1a5
Sam Hart <wsdh500@york.ac.uk>
2019-11-15 10:04:00 +0000

    Considered 'working'.

--- Mini Projects/MP3/Stage 1/main.c
+++ Mini Projects/MP3/Stage 1/main.c
@@ -2,13 +2,42 @@
 #include "lpc17xx_pinsel.h"
 #include "lpc17xx_i2c.h"
 #include "lpc_types.h"
+#include "lpc17xx_clkpwr.h"
+#include "lpc17xx_adc.h"
 #include "serial.h"
 
 void main(void)
 {
-	clear_serial();
+  uint32_t AdcData , last;
+  clear_serial();
 
-	debug_to_serial("Hello ADC\n\r");
+  debug_to_serial("Hello ADC\n\r");
 
-	while(1);
+  PINSEL_CFG_Type PinCfg;
+  PinCfg.Funcnum = 1;
+  PinCfg.OpenDrain = 0;
+  PinCfg.Pinmode = 2;
+  PinCfg.Portnum = 0;
+  PinCfg.Pinnum = 23;
+  PINSEL_ConfigPin(&PinCfg);
+
+  CLKPWR_ConfigPPWR (CLKPWR_PCONP_PCAD, ENABLE);
+  ADC_Init(LPC_ADC, 100000);
+  ADC_ChannelCmd(LPC_ADC, 0, ENABLE);
+  ADC_StartCmd(LPC_ADC, ADC_START_NOW);
+
+  last = 0;
+  AdcData = 0;
+  while(1)
+  {
+    AdcData = ADC_GlobalGetData(LPC_ADC);
+    if(AdcData & (1UL << 31))
+    {
+      ADC_StartCmd(LPC_ADC, ADC_START_NOW);
+      AdcData &= 0xFFF;
+      debug_to_serial("%u\n\r" , AdcData);
+    }
+  }
+
+  while(1);
 }
