17ffa5d1c5d1284af552327c47943e6b9c7a5ee5
Sam Hart <wsdh500@york.ac.uk>
2019-11-29 13:11:37 +0000

    Link against common files.

--- Mini Projects/MP3/Stage 1/i2c.h
+++
@@ -1,2 +0,0 @@
-void pins_i2c(void);
-Status write_i2c(I2C_M_SETUP_Type* setup);
--- Mini Projects/MP3/Stage 1/main.c
+++ Mini Projects/MP3/Stage 1/main.c
@@ -1,10 +1,6 @@
-#include "lpc17xx_uart.h"		// Central include files
-#include "lpc17xx_pinsel.h"
-#include "lpc17xx_i2c.h"
-#include "lpc_types.h"
-#include "lpc17xx_clkpwr.h"
-#include "lpc17xx_adc.h"
+#include "lpc17xx_uart.h"
 #include "serial.h"
+#include "adc.h"
 
 void main(void)
 {
@@ -13,32 +9,16 @@ void main(void)
 
   debug_to_serial("Hello ADC\n\r");
 
-  PINSEL_CFG_Type PinCfg;
-  PinCfg.Funcnum = 1;
-  PinCfg.OpenDrain = PINSEL_PINMODE_NORMAL;
-  PinCfg.Pinmode = PINSEL_PINMODE_TRISTATE;
-  PinCfg.Portnum = 0;
-  PinCfg.Pinnum = 23;
-  PINSEL_ConfigPin(&PinCfg);
-
-  CLKPWR_ConfigPPWR (CLKPWR_PCONP_PCAD, ENABLE);
-  ADC_Init(LPC_ADC, 100000);
-  ADC_ChannelCmd(LPC_ADC, 0, ENABLE);
-  ADC_StartCmd(LPC_ADC, ADC_START_NOW);
+  init_adc();
 
   last = 0;
   AdcData = 0;
   while(1)
   {
-    AdcData = ADC_GlobalGetData(LPC_ADC);
-    if(AdcData & (1UL << 31))
-    {
-      ADC_StartCmd(LPC_ADC, ADC_START_NOW);
-      AdcData &= 0xFFF;
-      if(AdcData != last)
-	debug_to_serial("%u\n\r" , AdcData);
-      last = AdcData;
-    }
+    AdcData = read_adc();
+    if(AdcData != last)
+      debug_to_serial("%u\n\r" , AdcData);
+    last = AdcData;
   }
 
   while(1);
