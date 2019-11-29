da17c43cc27f423fd90144e45d4bc7a80d955929
Sam Hart <wsdh500@york.ac.uk>
2019-11-29 13:03:46 +0000

    Link against common files.

--- Mini Projects/MP3/Stage 2/dac.h
+++ Mini Projects/MP3/Stage 2/dac.h
@@ -1,6 +1,1 @@
-
-void init_dac(void);
-void write_dac(uint32_t value);
-
-#define DAC_MIN 0
-#define DAC_MAX 0x3FF
+../common/dac.h
--- Mini Projects/MP3/Stage 2/rtc.h
+++
@@ -1,6 +0,0 @@
-
-void init_rtc(void);
-void enable_rtc(uint8_t enable);
-
-#define ENABLE 1
-#define DISABLE 0
