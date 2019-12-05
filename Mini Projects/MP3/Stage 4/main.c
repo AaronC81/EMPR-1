46163da0d80c09d8c7c75697d51e0a3abf220dc4
Sam Hart <wsdh500@york.ac.uk>
2019-12-05 11:05:52 +0000

    Smoothly run through all the values.

--- Mini Projects/MP3/Stage 4/main.c
+++ Mini Projects/MP3/Stage 4/main.c
@@ -1,41 +1,25 @@
 #include "lpc17xx_uart.h"
 #include "serial.h"
-#include "lpc17xx_rtc.h"
-#include "rtc.h"
 #include "pwm.h"
 
-void RTC_IRQHandler(void)
+void SysTick_Handler(void)
 {
-  static uint8_t counter = 0;
   static uint8_t match_value = 1;
   static uint8_t match_step = 1;
-
-  if(RTC_GetIntPending(LPC_RTC, RTC_INT_COUNTER_INCREASE))
-    RTC_ClearIntPending(LPC_RTC, RTC_INT_COUNTER_INCREASE);
-  if(RTC_GetIntPending(LPC_RTC, RTC_INT_ALARM))
-    RTC_ClearIntPending(LPC_RTC, RTC_INT_ALARM);
 
   uint8_t tmp;
   for(tmp = 1 ; tmp < 7 ; tmp++)
   {
     update_match(match_value , tmp);
     match_value += match_step;
-    debug_to_serial("Match value = %u\n\r" , match_value);
   }
-
-  if((counter++ & 3) == 0)
-    match_step++;
-
-  if(match_step == 10)
-    match_step = 1;
-
-  debug_to_serial("Match step = %u\n\r" , match_step);
 }
 
 
 void main(void)
 {
   uint8_t tmp , match_value;
+  uint32_t return_code;
 
   clear_serial();
 
@@ -50,8 +34,11 @@ void main(void)
     match_value += 5;
   }
 
-  init_rtc();
-  enable_rtc(RTC_ENABLE);
+  // milisecond granularity - 1kHz
+  return_code = SysTick_Config(SystemCoreClock / 10);
+  if(return_code != 0)
+    debug_to_serial("Error setting up SysTick interrupt.\n\r");
+
 
   while(1);
 }
